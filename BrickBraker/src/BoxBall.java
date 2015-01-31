c63d838ba7584df7e6a3addba3eda0c334f269f5
mbencheva <mbencheva@users.noreply.github.com>
2015-01-31 17:06:10 +0200

    Add box Ball

---
+++ BrickBraker/src/BoxBall.java
@@ -0,0 +1,16 @@
+public class BoxBall {
+	public int x,y;
+	public static final int BOX_SIZE = 20;
+	public BoxBall(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public boolean equals(Object obj){
+		if(obj instanceof BoxBall){
+			BoxBall b = (BoxBall) obj;
+			return (this.x==b.x && this.y==b.y);
+		}
+		return false;
+	}
+
+}
