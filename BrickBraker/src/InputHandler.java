95d6c25b11605010cb58d1a5f1c2f2da6f27eb65
Ivan Nikolov <ivan.nikolov@ipmediagroup.eu>
2015-02-01 01:22:56 +0200

    Update InputHandler.java

--- BrickBraker/src/InputHandler.java
+++ BrickBraker/src/InputHandler.java
@@ -1,4 +1,33 @@
 
-public class InputHandler {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class InputHandler implements KeyListener { 
+		
+	public InputHandler(Game game)	{
+		game.addKeyListener(this);
+	}
+	
+		@Override
+		public void keyPressed(KeyEvent e){
+			if(e.getKeyCode() == KeyEvent.VK_LEFT){
+				Game.board.setNextMove (-1);
+			}
+			if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+				Game.board.setNextMove (1);
+			}
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			Game.board.setNextMove (0);
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	
+	
+	
 }
