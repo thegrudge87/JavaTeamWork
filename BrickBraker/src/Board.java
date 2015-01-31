b758158213a668fe2631f9cc89cb5da68b45431b
Ivan Nikolov <ivan.nikolov@ipmediagroup.eu>
2015-02-01 01:49:35 +0200

    Update Board.java

--- BrickBraker/src/Board.java
+++ BrickBraker/src/Board.java
@@ -1,45 +1,34 @@
-
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
 
 
 public class Board {
-	int x = 0;
-	int xm = 1;
-	int size = 80;
+	private int x = 300;
+	private int xm = 1;
+	private int boardSize = 100;
 	
-	private Game game;
-	
-	
-	public Board (Game game){
-		this.game = game;
+		public Board (){
+		//this.game = game;
 	}
 	
 	public void move(){
-		if ((x+xm)>1&&(x+xm)<game.getWidth()-181){
+		if ((x+xm)>1&&(x+xm)< 600-boardSize){
 			x=x+xm;
 		}
 	}
 	
 	public void drawBoard (Graphics g){
 		g.setColor(Color.darkGray);
-		g.fillRect(x, 580, size, 15);
+		g.fillRect(x, 580, boardSize, 15);
 		
 	}
 	
-	public void keyPressed(KeyEvent e){
-		if(e.getKeyCode() == KeyEvent.VK_LEFT){
-			xm = -1;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-			xm = 1;
-		}
-	}
-	public void keyReleased(KeyEvent e) {
-			xm = 0;
+	public void setNextMove (int xm){
+			this.xm=xm;
 	}
 	
+	public void setSize (int size){
+		this.boardSize=size;
 }
-
-
+	
+}
